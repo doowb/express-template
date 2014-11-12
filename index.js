1847e4e7b2b4a39073ff8db11d6d4ea0f7f483b6
Brian Woodward <brian.woodward@gmail.com>
2014-11-11 23:40:50 -0500

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * express-template <https://github.com/doowb/express-template>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
